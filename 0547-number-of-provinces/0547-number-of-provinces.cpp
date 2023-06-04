2a9758d3d8ceef5b5c2c68cf9a224a7361874fa1
CodeBreaker231 <66872986+EswarBenarjee@users.noreply.github.com>
2023-06-04 21:52:54 +0530

    Time: 15 ms (99.46%), Space: 13.7 MB (87.38%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,58 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz), count(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            count--;
+        }
+    }
+
+    int getCount() {
+        return count;
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+    int count;
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int ans = 0;
+        
+        UnionFind uf(n);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(isConnected[i][i]==1 && isConnected[i][j]==1) uf.unionSet(i, j);
+            }
+        }
+
+        return uf.getCount();
+    }
+};
